058d9ae22ec814e81bfd67af467b7c6cc3f79aaa
Graham Burgess <stormmore@gmail.com>
2020-04-05 13:22:43 -0700

    playing with integration testing

---
+++ src/test/com/knowops/ci/jenkins/DeclarativeK8sLibJenkinsTest.groovy
@@ -0,0 +1,21 @@
+package com.knowops.ci.jenkins
+
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.TestInstance.Lifecycle
+import com.lesfurets.jenkins.unit.declarative.DeclarativePipelineTest
+
+@TestInstance(Lifecycle.PER_CLASS)
+class TestExampleDeclarativeJob extends DeclarativePipelineTest {\
+        @BeforeAll
+        void setUp() {
+          super.setUp()
+        }
+        @Test
+        void should_execute_without_errors() throws Exception {
+            def script = runScript("./.jenkins/development.groovy")
+            assertJobStatusSuccess()
+            printCallStack()
+        }
+}
