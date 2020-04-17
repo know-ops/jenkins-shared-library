e0e870bde489482eb77a9b37538c74290f3bd3c6
Graham Burgess <stormmore@gmail.com>
2020-04-16 20:52:57 -0700

    [clean] false completion; npe; etc

--- src/com/knowops/ci/jenkins/AgentSpec.groovy
+++ src/com/knowops/ci/jenkins/AgentSpec.groovy
@@ -107,7 +107,6 @@ class AgentSpec implements Serializable {
         if (this.kubernetes) {
             this.script.echo 'exec: starting: kubernetes'
             this.kubernetes.call()
-            this.script.echo 'exec: starting: kubernetes'
         } else if (this.node) {
             this.script.echo 'exec: starting: node'
             if (this.label) {
