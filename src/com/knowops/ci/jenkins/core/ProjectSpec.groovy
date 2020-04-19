3f1b744803c3f53a4dce309dbc4f77f08d40a143
Graham Burgess <stormmore@gmail.com>
2020-04-19 15:25:40 -0700

    [debug] no DSL method parseJson

--- src/com/knowops/ci/jenkins/core/ProjectSpec.groovy
+++ src/com/knowops/ci/jenkins/core/ProjectSpec.groovy
@@ -121,6 +121,13 @@ class ProjectSpec extends BaseSpec {
                         stage('Language') {
                             steps {
                                 container('linguist') {
+                                    echo """
+Name: ${project.name}
+Repository: ${project.repository}
+Language: ${project.language}
+Build Tool: ${project.buildTool}
+                                        """
+                                    }
                                     project.language = project.parseJson(
                                         sh(
                                             returnStdout: true,
