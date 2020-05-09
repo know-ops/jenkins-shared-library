bd28eb036a7e58202866e704eab20a0e8875fca2
Graham Burgess <stormmore@gmail.com>
2020-05-09 12:47:17 -0700

    [debug] checking core config

--- src/com/knowops/ci/jenkins/utils/Yaml.groovy
+++ src/com/knowops/ci/jenkins/utils/Yaml.groovy
@@ -3,7 +3,6 @@ package com.knowops.ci.jenkins.utils
 
 @Grab('org.yaml:snakeyaml:1.26')
 
-import org.yaml.snakeyaml.Yaml
 import org.yaml.snakeyaml.constructor.SafeConstructor
 
 class Yaml {
@@ -11,7 +10,7 @@ class Yaml {
     private final Yaml parser
 
     Yaml() {
-        parser = new Yaml(new SafeConstructor())
+        parser = new org.yaml.snakeyaml.Yaml(new SafeConstructor())
     }
 
     Map<String, Object> load(String yaml) {
