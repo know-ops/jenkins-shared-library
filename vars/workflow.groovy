8cc0d92050efa82f6b69bd2ccddd5d1726ee27bc
Graham Burgess <stormmore@gmail.com>
2020-05-10 12:47:51 -0700

    [debug] work

--- vars/workflow.groovy
+++ vars/workflow.groovy
@@ -7,6 +7,7 @@ import com.knowops.ci.jenkins.Project
 void call(String id, @DelegatesTo(strategy=Closure.DELEGATE_FIRST) Closure<?> work) {
 
     println id
+    println work
     println project.type
     println platform.nodes
 
